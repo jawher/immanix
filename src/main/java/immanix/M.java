64d3d9177145627c0f27728fae307fe8099e023f
Jawher Moussa <jawher.moussa@gmail.com>
2010-12-29 21:20:07 +0100

    Added (the missing) a class with a bunch of factory methods to crete common matchers

---
+++ src/main/java/immanix/M.java
@@ -0,0 +1,49 @@
+package immanix;
+
+import immanix.matchers.*;
+
+import javax.xml.stream.events.EndElement;
+import javax.xml.stream.events.StartElement;
+import javax.xml.stream.events.XMLEvent;
+
+public class M {
+    public static StartDocumentMatcher startDoc() {
+        return new StartDocumentMatcher();
+    }
+
+    public static EndDocumentMatcher endDoc() {
+        return new EndDocumentMatcher();
+    }
+
+    public static StaxMatcher<String> chars() {
+        return new CharsMatcher();
+    }
+
+    public static StaxMatcher<StartElement> start() {
+        return new StartElementMatcher();
+    }
+
+    public static StaxMatcher<StartElement> start(String name) {
+        return new NamedStartElementMatcher(name);
+    }
+
+    public static StaxMatcher<EndElement> end() {
+        return new EndElementMatcher();
+    }
+
+    public static StaxMatcher<EndElement> end(String name) {
+        return new NamedEndElementMatcher(name);
+    }
+
+    public static StaxMatcher<XMLEvent> success() {
+        return new SuccessMatcher();
+    }
+
+    public static <T> StaxMatcher<T> until(StaxMatcher<T> m) {
+        return new UntilMatcher<T>(m);
+    }
+
+    public static <T> StaxMatcher<T> untilButFinite(StaxMatcher<T> m, int giveUpBarrier) {
+        return new UntilButFiniteMatcher<T>(m, giveUpBarrier);
+    }
+}
